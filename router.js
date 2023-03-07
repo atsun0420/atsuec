c8548ba5ca0f0a92d5ca3cab5de1d8c2ba172fb4
atsun0420 <123521181+atsun0420@users.noreply.github.com>
2023-03-07 11:31:49 +0900

    Update router.js

--- router.js
+++ router.js
@@ -4,8 +4,6 @@ const express = require('express'); // Node.jsのフレームワーク、express
 const router = express.Router(); // ルーティング処理を記述するための準備
 const mysql = require('mysql'); // Node.jsからMySQLを操作するための準備
 
-
-
 // MySQLの接続情報
 const con = mysql.createConnection({
   host: 'localhost',
@@ -50,28 +48,50 @@ router.get('/', (req, res) => {
   res.render('comp.ejs');
 })
 
-// // ルートパラメータの値はparamsオブジェクトでフォームの値はbodyオブジェクトで受け取る
-// // 更新処理
-// .post('/update/:id',
-// (req, res, next) => {
-//   let id = req.params.id;
-//   let data = req.body.itemName;
-//   // 更新値未入力エラー(テキストボックスに更新する値が入っていなかった場合)
-//   if(data === "") {
-//     res.render('edit.ejs',{updItem : {updErr : '更新する値を入力してください。',id : id}}); // 更新値未入力エラー
-//     return;
+// //【お問い合わせフォーム(PC用)】
+// const contactBtn = document.getElementById("contactBtn"); // ボタン要素(PC用)を取得 
+// const gender = document.getElementsByName("gender"); // 性別ラジオボタンのname要素を取得
+// const selectage = document.getElementsByClassName("selectage")[0]; // 年代セレクトボックスのclass要素を取得
+// const txttitle = document.querySelector(".txttitle"); // ご意見タイトルのclass要素を取得
+// const txtgoiken = document.querySelector(".txtarea"); // ご意見のclass要素を取得
+
+// // エラーメッセージ表示の要素を取得
+// const errMsgGender = document.querySelector(".errMsgGender");
+// const errMsgAge = document.querySelector(".errMsgAge");
+// const errMsgTitle = document.querySelector(".errMsgTitle");
+// const errMsgGoiken = document.querySelector(".errMsgGoiken");
+
+// router.post('/contact', (req, res) => {
+//   if(genderSP[0].checked === false && genderSP[1].checked === false) {    
+//     errMsgGenderSP.textContent = "性別を選択してください";
+//   }else{
+//     errMsgGenderSP.textContent = "";
 //   }
-//   // テキストボックスに更新する値が入っている場合
-//   else if(data !== "") {
-//     con.query(
-//       'UPDATE items SET name = ? WHERE id = ?',
-//       [data,id], (error, results) => {
-//       if(error) {
-//           console.log(error);
-//         }
-//         res.render('complete.ejs',{compMsg : {updOk : '値を更新しました'}}); // 更新完了メッセージ  
-//       });
+
+//   if(selectageSP.options[selectageSP.selectedIndex].value === "") {
+//     errMsgAgeSP.textContent = "年代を選択してください";
+//   }else{
+//     errMsgAgeSP.textContent = "";
+//   }  
+
+//   if(!txttitleSP.value) {
+//     errMsgTitleSP.textContent = "タイトルを入力してください";      
+//   }else{
+//     errMsgTitleSP.textContent = "";
 //   }
+
+//   if(!txtgoikenSP.value) {
+//     errMsgGoikenSP.textContent = "ご意見を入力してください";
+//   }else{
+//     errMsgGoikenSP.textContent = "";
+//   }
+  
+//   if(errMsgGenderSP.textContent === "" && errMsgAgeSP.textContent === "" &&
+//     errMsgTitleSP.textContent === "" && errMsgGoikenSP.textContent === "") {
+//       res.render('comp.ejs');
+//   }
+
+  
 // })
 
 // // 新規登録処理
@@ -95,18 +115,5 @@ router.get('/', (req, res) => {
 //       }         
 //   });
 // })
-
-// 削除処理 /delete/:id URLでidを渡す(ルートパラメータという)
-// .post('/delete/:id', (req, res) => {
-//   let id = req.params.id;
-//   con.query(
-//   'DELETE FROM items WHERE id = ?',
-//   id,(error, results) => {  
-//     if (error) {
-//       console.log(error);
-//       }
-//       res.render('complete.ejs',{compMsg : {delOk : '値を削除しました'}}); // 削除完了メッセージ 
-//   });
-// });
 
 module.exports = router;
