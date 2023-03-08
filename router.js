f6f1a279601d36ddab8b5438590b153b29caf09d
atsun0420 <123521181+atsun0420@users.noreply.github.com>
2023-03-08 14:22:44 +0900

    Update router.js

--- router.js
+++ router.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const express = require('express'); // Node.jsのフレームワーク、expressを使用するための準備
 const router = express.Router(); // ルーティング処理を記述するための準備
 const mysql = require('mysql'); // Node.jsからMySQLを操作するための準備
@@ -10,7 +9,7 @@ const con = mysql.createConnection({
   port: 3306,
   user: 'root',
   password: 'password',
-  database: "list_app"
+  database: "userInfo"
 });
 
 // MySQLへ接続
@@ -48,72 +47,39 @@ router.get('/', (req, res) => {
   res.render('comp.ejs');
 })
 
-// //【お問い合わせフォーム(PC用)】
-// const contactBtn = document.getElementById("contactBtn"); // ボタン要素(PC用)を取得 
-// const gender = document.getElementsByName("gender"); // 性別ラジオボタンのname要素を取得
-// const selectage = document.getElementsByClassName("selectage")[0]; // 年代セレクトボックスのclass要素を取得
-// const txttitle = document.querySelector(".txttitle"); // ご意見タイトルのclass要素を取得
-// const txtgoiken = document.querySelector(".txtarea"); // ご意見のclass要素を取得
-
-// // エラーメッセージ表示の要素を取得
-// const errMsgGender = document.querySelector(".errMsgGender");
-// const errMsgAge = document.querySelector(".errMsgAge");
-// const errMsgTitle = document.querySelector(".errMsgTitle");
-// const errMsgGoiken = document.querySelector(".errMsgGoiken");
-
-// router.post('/contact', (req, res) => {
-//   if(genderSP[0].checked === false && genderSP[1].checked === false) {    
-//     errMsgGenderSP.textContent = "性別を選択してください";
-//   }else{
-//     errMsgGenderSP.textContent = "";
-//   }
-
-//   if(selectageSP.options[selectageSP.selectedIndex].value === "") {
-//     errMsgAgeSP.textContent = "年代を選択してください";
-//   }else{
-//     errMsgAgeSP.textContent = "";
-//   }  
-
-//   if(!txttitleSP.value) {
-//     errMsgTitleSP.textContent = "タイトルを入力してください";      
-//   }else{
-//     errMsgTitleSP.textContent = "";
-//   }
-
-//   if(!txtgoikenSP.value) {
-//     errMsgGoikenSP.textContent = "ご意見を入力してください";
-//   }else{
-//     errMsgGoikenSP.textContent = "";
-//   }
+// 新規登録処理
+ .post('/create',
+   (req, res, next) => {
+   let uname = req.body.userName;
+   let ugender = req.body.userGender;
+   let uage = req.body.userAge;
+   let utitle = req.body.userTitle;
+   let usentences = req.body.userSentences;
   
-//   if(errMsgGenderSP.textContent === "" && errMsgAgeSP.textContent === "" &&
-//     errMsgTitleSP.textContent === "" && errMsgGoikenSP.textContent === "") {
-//       res.render('comp.ejs');
-//   }
-
-  
-// })
-
-// // 新規登録処理
-// .post('/create',
-//   (req, res, next) => {
-//   let id = req.params.id;
-//   let data = req.body.itemName;  
-//   // insert～where not exists・・・データがあればinsertし、データがない場合はinsertしている     con.query('insert into items set ?',　通常のinsert文
-//   con.query('INSERT INTO items (name) SELECT ? WHERE NOT EXISTS (SELECT * FROM items WHERE name = ?)',
-//   [data,data],(error,results)=>{
-//       if(error) {
-//         console.log(error);
-//         return;
-//       } 
-//       //変数resultsをconsole.logで表示して、affectedRowプロパティの値を確認する
-//       if(results.affectedRows === 1) {        
-//         res.render('complete.ejs',{compMsg : {insOk : '値を登録しました'}}); // 新規登録完了メッセージ  
-//       } else {
-//         res.render('new.ejs',{emptyErr : {insErr : data + 'はすでに登録されています'}}); // 重複エラーメッセージ 
-//         return;
-//       }         
-//   });
-// })
+   con.query('insert into userreq(username,usergender,userage,usertitle,usersenetences) values(?,?,?,?,?)',
+   [uname,ugender,uage,utitle,usentences],
+  (error,results)=>{
+      if(error) {
+        console.log(error);
+        return;
+      }     
+        // ラジオボタンが未選択の場合、値がundefinedになるので、void 0 を記述
+        if(ugender === void 0) {
+         res.render('contact.ejs',{emptyErr : {insGenderErr : '性別を選択してください'}});
+        }
+        if(uage === "") {
+         res.render('contact.ejs',{emptyErr : {insAgeErr : '年齢を選択してください'}});
+        }
+        if(utitle === "") {
+         res.render('contact.ejs',{emptyErr : {insTitleErr : 'タイトルを入力してください'}});
+        }
+        if(usentences === "") {
+         res.render('contact.ejs',{emptyErr : {insSentencesErr : 'お問い合わせ内容を入力してください'}});
+        }
+        if(ugender !== "" && uage !== "" && utitle !== "" && usentences !== ""){
+          res.redirect('/comp');
+        }        
+     });         
+   });
 
 module.exports = router;
