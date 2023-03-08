fd433c2b296ef4e044033adc20759364b428601f
atsun0420 <123521181+atsun0420@users.noreply.github.com>
2023-03-08 14:22:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const express = require('express'); // Node.jsのフレームワーク、expressを使用するための準備
 const app = express(); // expressインスタンスを生成
 const router = require('./router'); // ルーティング処理を記述していく
@@ -9,5 +8,5 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use('/', router); // ルーティングのトップページを設定
 
-const port = 5500; // port番号：3000を指定
+const port = 5500; // port番号：5500を指定
 app.listen(5500);
