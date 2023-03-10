2c3b84397760a4368c0761ac372b6150e7721a3d
atsun0420 <123521181+atsun0420@users.noreply.github.com>
2023-03-10 16:30:01 +0900

    Update router.js

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express'); // Node.jsのフレームワーク、expressを使用するための準備
 const router = express.Router(); // ルーティング処理を記述するための準備
+const { check, validationResult } = require('express-validator'); // バリデーション処理の準備
 const mysql = require('mysql'); // Node.jsからMySQLを操作するための準備
 
 // MySQLの接続情報
@@ -18,7 +19,7 @@ con.connect((err) => {
     console.log('error connectiong:' + err.stack);
     return;
   }
-  console.log('create start!');
+  console.log('SQL start!');
 });
 
 /* 画面を表示したいとき：getを使用  データベースを変更したいとき：postを使用 */
@@ -47,8 +48,28 @@ router.get('/', (req, res) => {
   res.render('comp.ejs');
 })
 
+// バリデーション(express-validator使用)
+// const insValidator = [
+//   check('userGender').not().isEmpty().withMessage("性別：性別を選択して下さい。"),
+//   check('userAge').not().isEmpty().withMessage("年齢：年代を選択して下さい。"),
+//   check('userTitle').not().isEmpty().withMessage("タイトル：タイトルを入力して下さい。"),
+//   check('userSentences').not().isEmpty().withMessage("お問い合わせ内容：内容を入力して下さい。"),
+//   // エラー処理まで行ってしまう
+//   async(req, res, next) => {
+//       // エラーの状況を取得
+//       const errors = validationResult(req);
+//       // エラーが発生していたら内容を取得して画面に表示
+//       if (!errors.isEmpty()) {
+//           res.render('contact.ejs', {errors: errors.array()});
+//           return;
+//       }
+//       // エラーがなければnextメソッド呼び出し
+//       next();
+//   },
+// ];
+
 // 新規登録処理
- .post('/create',
+ router.post('/create',
    (req, res, next) => {
    let uname = req.body.userName;
    let ugender = req.body.userGender;
@@ -58,28 +79,19 @@ router.get('/', (req, res) => {
   
    con.query('insert into userreq(username,usergender,userage,usertitle,usersenetences) values(?,?,?,?,?)',
    [uname,ugender,uage,utitle,usentences],
-  (error,results)=>{
-      if(error) {
-        console.log(error);
-        return;
-      }     
-        // ラジオボタンが未選択の場合、値がundefinedになるので、void 0 を記述
-        if(ugender === void 0) {
-         res.render('contact.ejs',{emptyErr : {insGenderErr : '性別を選択してください'}});
+   (error,results)=>{
+        if(error) {
+          console.log(error);
+          return;
         }
-        if(uage === "") {
-         res.render('contact.ejs',{emptyErr : {insAgeErr : '年齢を選択してください'}});
-        }
-        if(utitle === "") {
-         res.render('contact.ejs',{emptyErr : {insTitleErr : 'タイトルを入力してください'}});
-        }
-        if(usentences === "") {
-         res.render('contact.ejs',{emptyErr : {insSentencesErr : 'お問い合わせ内容を入力してください'}});
-        }
+        // ラジオボタンが未選択の場合は特殊処理、値がundefinedになるので、void 0 を記述
+        // if(ugender === void 0) {
+        //  res.render('contact.ejs',{emptyErr : {insGenderErr : '性別を選択してください'}});
+        // }
+        // 全ての項目に値が入力または選択されていたら完了画面へ遷移
         if(ugender !== "" && uage !== "" && utitle !== "" && usentences !== ""){
           res.redirect('/comp');
         }        
      });         
-   });
-
+  });
 module.exports = router;
